ad855d14e5064fb79c665c83f652102ff9f76207
mrhrifat <mrhrifat383759@gmail.com>
2021-11-25 12:47:03 +0600

    Upload Posts Page

---
+++ src/Components/Lesson1/pages/Posts.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import PostNav from './Nav/PostNav'
+
+class Posts extends Component {
+    render() {
+        return (
+            <div>
+                <PostNav />
+            </div>
+        )
+    }
+}
+
+export default Posts
