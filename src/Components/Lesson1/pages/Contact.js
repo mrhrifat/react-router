48c97f7f0b36d044dd8f2b0f5ddc5b890308408f
mrhrifat <mrhrifat383759@gmail.com>
2021-11-25 12:44:44 +0600

    Contact Page

---
+++ src/Components/Lesson1/pages/Contact.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Contact extends Component {
+    render() {
+        return (
+            <div>
+                <h6>I am Contact</h6>
+            </div>
+        )
+    }
+}
+
+export default Contact
