fe031077bcf3a84381fb11340ae9f9a11b62b91f
mrhrifat <mrhrifat383759@gmail.com>
2021-11-25 12:37:45 +0600

    Create for Import All Lesson

--- src/Components/Root.jsx
+++ src/Components/Root.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import Lesson1 from './Lesson1/Lesson1'
 
 class Root extends Component {
     render() {
         return (
             <div>
-                
+                <Lesson1 />
             </div>
         )
     }
