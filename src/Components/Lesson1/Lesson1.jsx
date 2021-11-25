5f09d777c3d41a6bdf570fb96429b49c1b425b1f
mrhrifat <mrhrifat383759@gmail.com>
2021-11-25 12:37:08 +0600

    Root Component of Lesson 1

---
+++ src/Components/Lesson1/Lesson1.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import Posts from './pages/Posts'
+import Contact from './pages/Contact'
+import MainNav from './pages/MainNav'
+import Hello from './pages/Hello'
+import Post from './pages/Post'
+
+class Lesson1 extends Component {
+    render() {
+        return (
+            <BrowserRouter>
+                <div>
+                    <h4>Lesson 1</h4>
+                    <MainNav />
+                    <Routes>
+                        <Route path='/' element={<Navigate to='/hello' />} />
+                        <Route path='/hello/*' element={<Hello />}>
+                            <Route path='learn' element={<p>I am learning it now</p>} />
+                        </Route>
+                        <Route path='/posts/*' element={<Posts />} />
+                        <Route path='/posts/:postId' element={<Post />} />
+                        <Route path='/contact' element={<Contact />} />
+                    </Routes>
+                </div>
+            </BrowserRouter >
+        )
+    }
+}
+
+export default Lesson1
