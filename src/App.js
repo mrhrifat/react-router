e0fd046710e7039a0702e5b40d4fe42abaae6eac
mrhrifat <mrhrifat383759@gmail.com>
2021-11-25 12:07:03 +0600

    Add Root Components for devide Lesson

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Root from "./Components/Root";
 function App() {
   return (
-    <div className="App">
-     <Root/>      
+    <div className="container">
+      <Root />
     </div>
   );
 }
