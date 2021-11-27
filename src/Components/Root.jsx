a869eff00eeb71c881aad620cec6f3ef4edcaa30
mrhrifat <mrhrifat383759@gmail.com>
2021-11-27 12:10:16 +0600

    Import Lesson2 & Disabled Lesson1

--- src/Components/Root.jsx
+++ src/Components/Root.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import Lesson1 from './Lesson1/Lesson1'
+import Lesson2 from './Lesson2/Lesson2'
 
 class Root extends Component {
     render() {
         return (
             <div>
-                <Lesson1 />
+                {/* <Lesson1 /> */}
+                <Lesson2 />
             </div>
         )
     }
